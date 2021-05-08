92d2633514330b2fe5aea21676841981da002d02
alexlusco <44949344+alexlusco@users.noreply.github.com>
2021-05-08 18:53:22 -0400

    SVM modeling

--- code/random_forest.R
+++ code/random_forest.R
@@ -1,14 +1,62 @@
 library(tidytext)
+library(tidyverse)
 library(tidymodels)
+library(textrecipes)
+library(tidypredict)
 library(here)
+
+set.seed(1234)
 
 axon_df <- read_csv("data/axon-nlp-es-master-df.csv")
 
 axon_df <- axon_df %>%
-  select(car_value, event_date, word_count)
+  mutate(year = format(as.Date(event_date, "%Y-%m-%d"), format = "%Y"),
+         text = str_remove_all(text, "'"),
+         text = tolower(text)) %>%
+  mutate(across(c(car_value, year), as.numeric)) %>%
+  select(car_value, event_date, year, text)
 
-rand_forest(mode = "regression", mtry = 2, trees = 1000) %>%
-  set_engine("randomForest") %>%
-  fit(
-    car_value ~ event_date + word_count, data = axon_df
-  )
+axon_split <- axon_df %>%
+  initial_split()
+
+axon_train <- training(axon_split)
+axon_text <- testing(axon_split)
+
+axon_rec <- recipe(car_value ~ text + year, data = axon_train) %>%
+  step_tokenize(text) %>%
+  step_tokenfilter(text, max_tokens = 1e3) %>%
+  step_stopwords(text, stopword_source = "snowball") %>%
+  step_tfidf(text) %>%
+  step_normalize(all_predictors())
+
+axon_prep <- prep(axon_rec)
+axon_bake <- bake(axon_prep, new_data = NULL)
+
+dim(axon_bake)
+
+axon_wf <- workflow() %>%
+  add_recipe(axon_rec)
+
+# Support vector machine
+svm_spec <- svm_linear() %>%
+  set_mode("regression") %>%
+  set_engine("LiblineaR")
+
+svm_fit <- workflow() %>%
+  add_recipe(axon_rec) %>%
+  add_model(svm_spec)
+
+svm_fit %>%
+  fit(data = axon_train) %>%
+  pull_workflow_fit() %>%
+  tidy() %>%
+  arrange(-estimate) %>% # positive CARs
+  print(n = 150)
+
+svm_fit %>%
+  fit(data = axon_train) %>%
+  pull_workflow_fit() %>%
+  tidy() %>%
+  arrange(estimate) %>% # negative CARs
+  print(n = 100)
+
